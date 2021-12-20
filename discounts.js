572ed0fc11e8e16e2271a0e77733e15ca568d033
Juan Lopez <jlopezp07@gmail.com>
2021-12-20 06:16:54 -0500

    Finished function with arrays and objects

--- discounts.js
+++ discounts.js
@@ -1,13 +1,12 @@
 const precioOriginal = 796112.95;
 const descuento = 20;
 
-function calcularPrecioConDescuento (precio, descuento) {
+function calcularPrecioConDescuento(precio, descuento) {
   const porcentajeDescuento = 100 - descuento;
-  const precioConDescuento = (precio * porcentajeDescuento) / 100;  
+  const precioConDescuento = (precio * porcentajeDescuento) / 100;
 
   return precioConDescuento;
 }
-
 
 // console.log({
 //     precioOriginal,
@@ -25,11 +24,43 @@ function calculateDiscount() {
   const totalDiscount = 100 - valueDiscount;
   const totalPrice = (valuePrice * totalDiscount) / 100;
   const result = document.getElementById("finalPrice");
-  result.innerText = "El precio final con descuento es $" + totalPrice; 
-
+  result.innerText = "El precio final con descuento es $" + totalPrice;
 }
 
 function calculateDiscountCoupon() {
-  const coupon = document.getElementById("coupons");
-  const couponValue = coupon.value;
+  const coupons = [
+    {
+      name: "CuponPlatzi20",
+      discount: 20,
+    },
+    {
+      name: "CuponPlatzi25",
+      discount: 25,
+    },
+    {
+      name: "CuponPlatzi30",
+      discount: 30,
+    },
+    {
+      name: "CuponPlatzi50",
+      discount: 50,
+    },
+  ];
+
+  const userCouponInput = document.getElementById("coupon").value;
+  const couponValid = coupons.find(function (coupon) {
+    //se retorna del array coupons el valor de coupon.name
+    return coupon.name === userCouponInput;
+  });
+
+  if (!couponValid) {
+    finalDiscountPrice.innerText =
+      "El cupón ingresado no es válido! Por favor intenta nuevamente.";
+  } else {
+    const discount = couponValid.discount;
+    const originalPrice = document.getElementById("originalPrice").value;
+    const finalPrice = (originalPrice * (100 - discount)) / 100;
+    finalDiscountPrice.innerText =
+      "El precio final con el cupón " + userCouponInput + " es $" + finalPrice;
+  }
 }
