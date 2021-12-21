b4cc56efba9110f676dbf5eeaf90150757642824
Juan Lopez <jlopezp07@gmail.com>
2021-12-21 08:03:51 -0500

    Finished median function

--- arimetica.js
+++ arimetica.js
@@ -1,29 +1,27 @@
-function calcularMedia (lista) {
+function calcularMedia(lista) {
   // let sumaLista = 0;
   // for (let i = 0; i < lista.length; i++) {
   //   sumaLista = sumaLista + lista[i];
   // }
-  const sumaLista = lista.reduce(
-    function (valorAcumulado = 0, nuevoElemento) {
-      return valorAcumulado + nuevoElemento;
-    }
-  )
+  const sumaLista = lista.reduce(function (valorAcumulado = 0, nuevoElemento) {
+    return valorAcumulado + nuevoElemento;
+  });
   const promedioLista = sumaLista / lista.length;
   return promedioLista;
 }
 
-const lista1 = [
-  100,
-  200,
-  500,
-  800,
-  4000000,
-];
 
+//Calcular mediana
+const lista1 = [100, 4000000, 200, 500, 800];
+
+lista1.sort(function (a, b) {
+  return a - b;
+});
 const mitadLista = parseInt(lista1.length / 2);
 
-function esPar (numero) {
-  if (numero % 2 === 0) {// % modulo
+function esPar(numero) {
+  if (numero % 2 === 0) {
+    // % modulo
     return true;
   } else {
     return false;
@@ -35,11 +33,10 @@ let mediana;
 if (esPar(lista1.length)) {
   const elemento1 = lista1[mitadLista - 1];
   const elemento2 = lista1[mitadLista];
-  const media = calcularMedia([
-    elemento1,
-    elemento2,
-  ]);
+  const media = calcularMedia([elemento1, elemento2]);
   mediana = media;
 } else {
-  mediana = lista1[mitadLista] ;
+  mediana = lista1[mitadLista];
 }
+
+//Calcular moda
